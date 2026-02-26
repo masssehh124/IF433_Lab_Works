39d2ed2165fc00acd16203b03f8a0c43fbd4b3dd
farre <farrelra202004@gmail.com>
2026-02-26 22:36:37 +0700

    week01: implement null safety check

--- src/oop_00000069400_FarrelRamadaAlladines/week01/GradingSystem.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week01/GradingSystem.kt
@@ -17,5 +17,11 @@ fun main() {
 
     println("Grade kamu: $grade")
     println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
 }
 
