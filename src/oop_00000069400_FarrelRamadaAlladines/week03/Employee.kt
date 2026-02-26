9087b4da62e11ec079518c6199d891beb9a41608
farre <farrelra202004@gmail.com>
2026-02-26 23:29:41 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000069400_FarrelRamadaAlladines/week03/Employee.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week03/Employee.kt
@@ -1,5 +1,10 @@
 package oop_00000069400_FarrelRamadaAlladines.week03
 
 class Employee(val name: String) {
-    val salary: Int = 0
+    var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // ERROR: Rekursif (memanggil dirinya sendiri)
+            this.salary = value
+        }
 }
