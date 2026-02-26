4ac39af6e03057c24ff7bd528b4ec08e0f1b3197
farre <farrelra202004@gmail.com>
2026-02-26 23:15:56 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000069400_FarrelRamadaAlladines/week01/GameStore.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week01/GameStore.kt
@@ -3,19 +3,21 @@ package oop_00000069400_FarrelRamadaAlladines.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
+    val userNote: String? = null // Simulasi data null
 
     val discount = calculateDiscount(price)
     val finalPrice = price - discount
 
-    // Pemanggilan wajib menggunakan Named Arguments
-    printReceipt(title = gameTitle, originalPrice = price, finalPrice = finalPrice)
+    printReceipt(title = gameTitle, originalPrice = price, finalPrice = finalPrice, note = userNote)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-fun printReceipt(title: String, originalPrice: Int, finalPrice: Int) {
+fun printReceipt(title: String, originalPrice: Int, finalPrice: Int, note: String?) {
     println("--- Struk SteamKW ---")
     println("Game: $title")
     println("Harga Asli: Rp $originalPrice")
     println("Harga Akhir: Rp $finalPrice")
+    // Elvis Operator: Jika null munculkan pesan default
+    println("Catatan: ${note ?: "Tidak ada catatan"}")
 }
