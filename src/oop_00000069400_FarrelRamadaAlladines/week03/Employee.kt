1cff87b593e0037ec3e91eb9eb7dda2b1215549d
farre <farrelra202004@gmail.com>
2026-02-26 23:31:29 +0700

    week03: fix setter using backing field

--- src/oop_00000069400_FarrelRamadaAlladines/week03/Employee.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week03/Employee.kt
@@ -3,8 +3,11 @@ package oop_00000069400_FarrelRamadaAlladines.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            // ERROR: Rekursif (memanggil dirinya sendiri)
-            this.salary = value
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
