6fff47023601014a12975f1f3040b25cb5d167d6
farre <farrelra202004@gmail.com>
2026-02-26 23:12:48 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000069400_FarrelRamadaAlladines/week01/GameStore.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_00000069400_FarrelRamadaAlladines.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
+
+// Fungsi menghitung diskon dengan Expression Body
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
