492662e62b30d123cfcbcc2b9485eff47c1180a8
farre <farrelra202004@gmail.com>
2026-02-27 23:48:45 +0700

    week02: add secondary constructor

--- src/oop_00000069400_FarrelRamadaAlladines/week02/Student.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week02/Student.kt
@@ -1,8 +1,14 @@
 package oop_00000069400_FarrelRamadaAlladines.week02
 
 class Student (val name: String, val nim: String, var major: String) {
+
+    // Secondary Constructor
+    // Wajib memanggil Primary Constructor menggunakan 'this()'
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
-        // Validasi Sederhana: Cek panjang NIM
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
