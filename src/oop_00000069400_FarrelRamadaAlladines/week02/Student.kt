880ae88615b93292716e653b189d1dc6b53fab26
farre <farrelra202004@gmail.com>
2026-02-27 23:50:29 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000069400_FarrelRamadaAlladines/week02/Student.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week02/Student.kt
@@ -1,9 +1,12 @@
 package oop_00000069400_FarrelRamadaAlladines.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String,
+    val gpa: Double = 0.0
+) {
 
-    // Secondary Constructor
-    // Wajib memanggil Primary Constructor menggunakan 'this()'
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
@@ -11,7 +14,6 @@ class Student (val name: String, val nim: String, var major: String) {
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
