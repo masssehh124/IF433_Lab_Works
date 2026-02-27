447241a1a872df527bd0bf0c3f2366f8b951c90c
farre <farrelra202004@gmail.com>
2026-02-27 23:37:30 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000069400_FarrelRamadaAlladines/week04/Main.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week04/Main.kt
@@ -1,23 +1,13 @@
 package oop_00000069400_FarrelRamadaAlladines.week04
 
 fun main() {
-    println("--- Testing Vehicle ---")
-    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
-    generalVehicle.honk()
-    generalVehicle.accelerate()
+    println("\n--- Testing Employee Hierarchy ---")
 
-    println("\n--- Testing Car ---")
-    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()
-    myCar.honk()
-    myCar.accelerate()
+    val manager = Manager(name = "Budi", baseSalary = 8000000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
 
-    println("\n--- Testing Electric Car ---")
-    val myEV = ElectricCar(brand = "Tesla Model 3", numberOfDoors = 4, batteryCapacity = 85)
-
-    myEV.accelerate()
-
-    myEV.honk()
-
-    myEV.openTrunk()
+    val developer = Developer(name = "Andi", baseSalary = 6000000, programmingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
