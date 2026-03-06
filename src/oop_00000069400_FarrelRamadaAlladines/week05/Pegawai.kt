e0bc796379e52fc894d34d24f3692bc1a03650c8
farre <farrelra202004@gmail.com>
2026-03-06 15:12:08 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000069400_FarrelRamadaAlladines.week05
+
+abstract class Pegawai(val nama: String) {
+    //Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja ()
+}
