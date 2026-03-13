806b0abf6694794562cdb50b152ffd5b96ff9056
farre <farrelra202004@gmail.com>
2026-03-13 15:18:35 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000069400_FarrelRamadaAlladines.week06
+
+interface Camera {
+    fun turnon() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnon() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
