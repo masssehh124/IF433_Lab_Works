d239ef498e3bd75c55e152b9b5f257d40e8686ac
farre <farrelra202004@gmail.com>
2026-03-27 23:05:54 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000069400_FarrelRamadaAlladines.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(30),
+    RARE(15),
+    EPIC(5),
+    LEGENDARY(1)
+}
