c6e2490a619cfa1bd6cf4bd9c09e7a4f4dda71c0
farre <farrelra202004@gmail.com>
2026-03-27 22:47:22 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000069400_FarrelRamadaAlladines/week07/Main.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week07/Main.kt
@@ -31,8 +31,10 @@ fun main() {
 
     println("\n=== TEST SEALED CLASS ===")
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-    val uiMessage = when(response) {
+    val uiMessage = when (response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
+    println(uiMessage)
 }
