5e72093c73dd6616339c14a0ca0d8088da0f69c9
farre <farrelra202004@gmail.com>
2026-03-27 22:36:44 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_00000069400_FarrelRamadaAlladines/week07/DatabaseManager.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week07/DatabaseManager.kt
@@ -1,11 +1,9 @@
 package oop_00000069400_FarrelRamadaAlladines.week07
 
-class DatabaseManager {
-    object DatabaseManager {
-        var connectionStatus: String = "Disconnected"
-        fun connect() {
-            connectionStatus = "Connected to Server"
-            println("Database is ready.")
-        }
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
     }
 }
---
+++ src/oop_00000069400_FarrelRamadaAlladines/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_00000069400_FarrelRamadaAlladines.week07
+
+class RegularUser (val name: String, val age: Int)
