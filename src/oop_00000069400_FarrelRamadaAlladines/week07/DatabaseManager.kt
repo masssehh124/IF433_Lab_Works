77d86b6e55de2fc633d44e9d71b8573574a4d2c0
farre <farrelra202004@gmail.com>
2026-03-27 22:19:19 +0700

    week07: create singleton Database Manager

--- src/oop_00000069400_FarrelRamadaAlladines/week07/DatabaseManager.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week07/DatabaseManager.kt
@@ -3,7 +3,6 @@ package oop_00000069400_FarrelRamadaAlladines.week07
 class DatabaseManager {
     object DatabaseManager {
         var connectionStatus: String = "Disconnected"
-
         fun connect() {
             connectionStatus = "Connected to Server"
             println("Database is ready.")
