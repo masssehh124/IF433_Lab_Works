f346d28689fd2f1417b243374065c22ab621676e
farre <farrelra202004@gmail.com>
2026-04-17 21:23:41 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000069400_FarrelRamadaAlladines.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_0K";
+    }
+}
