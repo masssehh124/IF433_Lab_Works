2d1464266c7711875c3e8e3ed548a3804d596d78
farre <farrelra202004@gmail.com>
2026-04-17 21:26:06 +0700

    week08: simulate unit test failure using !!

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week08/UserTest.kt
@@ -0,0 +1,19 @@
+package oop_00000069400_FarrelRamadaAlladines.week08
+
+object DatabaseMock {
+    fun findUser(id: Int): UserProfile? {
+        return if (id == 1) UserProfile("TestUser", "test@test.com") else null
+    }
+
+    fun runMockUnitTest() {
+        println("\n=== RUNNING UNIT TEST ===")
+        val testUser = findUser(1)
+
+        // Di Unit Testing, penggunaan !! sangat lazim karena jika null, test HARUS gagal
+        val initial = testUser!!.name.substring(0, 1)
+
+        // check() adalah fungsi bawaan Kotlin untuk assertion
+        check(initial == "T") { "Test Failed! Initial is wrong." }
+        println("Test Passed: Initial is T")
+    }
+}
