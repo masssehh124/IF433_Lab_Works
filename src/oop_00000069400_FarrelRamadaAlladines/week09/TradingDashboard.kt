7d254a2dd4f70db66da75c788971ba22d9f78cc8
farre <farrelra202004@gmail.com>
2026-04-24 15:26:44 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000069400_FarrelRamadaAlladines/week09/TradingDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week09/TradingDashboard.kt
@@ -22,6 +22,10 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
+
+    worstPerformersString.forEach { println(it) }
+    println("Unique Pairs: $uniquePairs")
 }
