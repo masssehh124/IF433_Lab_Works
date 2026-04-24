a64b0f76af2844097e2bb9e93744777dd3e109b4
farre <farrelra202004@gmail.com>
2026-04-24 15:25:38 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000069400_FarrelRamadaAlladines/week09/TradingDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week09/TradingDashboard.kt
@@ -22,4 +22,6 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
