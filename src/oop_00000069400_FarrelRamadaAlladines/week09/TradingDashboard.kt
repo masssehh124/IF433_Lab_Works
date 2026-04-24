4b5a579ac14d6063a35a9ff33ca367a2472b0bf0
farre <farrelra202004@gmail.com>
2026-04-24 15:19:01 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000069400_FarrelRamadaAlladines.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 25, 30.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, -12.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 15, 5.0, "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 10, -2.5, "OPEN")
+    )
+}
