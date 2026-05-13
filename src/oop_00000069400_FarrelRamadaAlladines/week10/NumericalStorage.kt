1078170d8e6cd0f45c1f9cfed98d5ddb7bb668bf
farre <farrelra202004@gmail.com>
2026-05-13 22:29:43 +0700

    week10: implement where clause for comparable constraints

--- src/oop_00000069400_FarrelRamadaAlladines/week10/NumericalStorage.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/NumericalStorage.kt
@@ -1,8 +1,12 @@
 package oop_00000069400_FarrelRamadaAlladines.week10
 
-// Class MathBox hanya menerima tipe data yang merupakan turunan dari Number
 class MathBox<T: Number>(val value1: T, val value2: T) {
     fun sum(): Double {
         return value1.toDouble() + value2.toDouble()
     }
+}
+
+// Fungsi getMax mengharuskan T memenuhi kontrak Comparable agar bisa dibandingkan
+fun <T> getMax(a: T, b: T): T where T: Comparable<T> {
+    return if (a > b) a else b
 }
