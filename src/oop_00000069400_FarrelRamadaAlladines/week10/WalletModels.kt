ec7dc8fc3faa19830830217271b67b57c4fc342f
farre <farrelra202004@gmail.com>
2026-05-13 22:32:02 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/WalletModels.kt
@@ -0,0 +1,7 @@
+package oop_00000069400_FarrelRamadaAlladines.week10
+
+// Data class untuk merepresentasikan entitas koin
+data class Coin(val name: String, val balance: Double)
+
+// Data class untuk merepresentasikan entitas transaksi
+data class Transaction(val id: String, val amount: Double)
