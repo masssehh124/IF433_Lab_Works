941493ecac295a0ec55c45718280624c46573788
farre <farrelra202004@gmail.com>
2026-05-13 22:40:16 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
@@ -8,4 +8,11 @@ fun main() {
     coinRepo.add(Coin("USDT", 1000.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
+
+    println("Status: ${response.status}")
+
+    println("--- Coin List ---")
+    response.data.forEach { coin ->
+        println("Coin: ${coin.name}, Balance: ${coin.balance}")
+    }
 }
