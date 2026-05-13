8aedff0ea90164ba5f0ca9001c473d986a259d68
farre <farrelra202004@gmail.com>
2026-05-13 22:38:04 +0700

    week10: (task) populate coin repository

--- src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_00000069400_FarrelRamadaAlladines.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    // Menambahkan data koin ke dalam repository
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
