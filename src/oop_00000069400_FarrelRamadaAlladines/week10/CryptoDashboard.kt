3bf576aed052243ed566d262ba14baecb9c3344d
farre <farrelra202004@gmail.com>
2026-05-13 22:39:13 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/CryptoDashboard.kt
@@ -3,8 +3,9 @@ package oop_00000069400_FarrelRamadaAlladines.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
 
-    // Menambahkan data koin ke dalam repository
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.0))
     coinRepo.add(Coin("USDT", 1000.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
