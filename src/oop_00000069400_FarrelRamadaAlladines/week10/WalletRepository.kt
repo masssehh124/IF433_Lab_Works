44e52f9b01f836295967acd23c94e95240baed54
farre <farrelra202004@gmail.com>
2026-05-13 22:35:44 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000069400_FarrelRamadaAlladines/week10/WalletRepository.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/WalletRepository.kt
@@ -1,6 +1,6 @@
 package oop_00000069400_FarrelRamadaAlladines.week10
 
-class WalletRepository<T> {
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
 
     fun add(item: T) {
@@ -9,5 +9,10 @@ class WalletRepository<T> {
 
     fun getAll(): List<T> {
         return items
+    }
+
+    // Fungsi pencarian sederhana menggunakan filter
+    fun search(predicate: (T) -> Boolean): List<T> {
+        return items.filter(predicate)
     }
 }
