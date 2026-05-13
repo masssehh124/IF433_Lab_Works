594b8d7b4d9922c9c94ddf24e8635c8cfed843d6
farre <farrelra202004@gmail.com>
2026-05-13 22:20:10 +0700

    week10: implement PairBox in main function

--- src/oop_00000069400_FarrelRamadaAlladines/week10/Main.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week10/Main.kt
@@ -2,13 +2,13 @@ package oop_00000069400_FarrelRamadaAlladines.week10
 
 fun main() {
     println("=== TEST GENERIC CLASS ===")
-
-    // Instansiasi Box dengan tipe Int
     val intBox = Box(100)
-
-    // Instansiasi Box dengan tipe String
     val stringBox = Box("Generics in Kotlin")
-
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
+
+    println("\n=== TEST MULTIPLE PARAMETERS ===")
+    // Menggunakan PairBox dengan tipe String untuk Key dan Int untuk Value
+    val itemPrice = PairBox("Bitcoin", 65000)
+    println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
 }
