97ab7068119e8c2bfe86c5570e458876d358aa78
farre <farrelra202004@gmail.com>
2026-05-14 21:47:49 +0700

    week11: test extension functions in main

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000069400_FarrelRamadaAlladines.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
