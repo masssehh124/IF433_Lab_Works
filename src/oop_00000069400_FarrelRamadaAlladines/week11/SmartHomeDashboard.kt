5a1a5fe79ee41771f1bb9a49c8ec99a59ad479bf
farre <farrelra202004@gmail.com>
2026-05-14 22:03:05 +0700

    week11: (task) implement device search with let

--- src/oop_00000069400_FarrelRamadaAlladines/week11/SmartHomeDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week11/SmartHomeDashboard.kt
@@ -24,4 +24,10 @@ fun main() {
     homeDevices.add(acUnit)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    // Pencarian Aman dengan let [cite: 153]
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
