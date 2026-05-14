82de0a63cdca050018a66c15c3daf96d8c054100
farre <farrelra202004@gmail.com>
2026-05-14 22:04:09 +0700

    week11: (task) calculate total power usage

--- src/oop_00000069400_FarrelRamadaAlladines/week11/SmartHomeDashboard.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week11/SmartHomeDashboard.kt
@@ -34,4 +34,9 @@ fun main() {
         println("=== SMART HOME DASHBOARD ===")
         println("Jumlah perangkat terdaftar: ${this.size}")
     }
+
+    val totalPower = homeDevices.run {
+        sumOf { it.powerLoad }
+    }
+    println("Total penggunaan daya: $totalPower Watt")
 }
