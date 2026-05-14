e008b8f64b75170d722029832b7f7bc04d6b06f0
farre <farrelra202004@gmail.com>
2026-05-14 21:48:40 +0700

    week11: implement let function for null safety

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week11/ScopeLetRun.kt
@@ -0,0 +1,14 @@
+package oop_00000069400_FarrelRamadaAlladines.week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    // Mengeksekusi blok hanya jika objek tidak null
+    val length = name?.let {
+        println("Nama terdeteksi: $it") // 'it' merujuk pada name
+        it.length // Mengembalikan nilai expression terakhir
+    }
+
+    println("Panjang nama: $length")
+}
