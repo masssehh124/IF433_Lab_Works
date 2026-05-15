85125e90d93c8249a375683c42f76f9db4a25010
farre <farrelra202004@gmail.com>
2026-05-15 14:49:54 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
@@ -0,0 +1,7 @@
+package oop_00000069400_FarrelRamadaAlladines.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    return availableGram
+}
