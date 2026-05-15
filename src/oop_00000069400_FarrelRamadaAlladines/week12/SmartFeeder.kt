57b54aa75b21bd5dcbbe076b6c65cbdb2d63e4e3
farre <farrelra202004@gmail.com>
2026-05-15 14:54:40 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
@@ -30,4 +30,8 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    val feedingResult = runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
