4a3535808909c9873f7fefaafea52993842a675f
farre <farrelra202004@gmail.com>
2026-05-15 14:50:41 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
@@ -3,5 +3,9 @@ package oop_00000069400_FarrelRamadaAlladines.week12
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
 
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
     return availableGram
 }
