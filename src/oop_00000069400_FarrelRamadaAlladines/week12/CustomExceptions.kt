163d4aba5af184c4db17dfb0a1fefd88084451cc
farre <farrelra202004@gmail.com>
2026-05-15 14:43:24 +0700

    week12: implement throw keyword for validation

--- src/oop_00000069400_FarrelRamadaAlladines/week12/CustomExceptions.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week12/CustomExceptions.kt
@@ -1,7 +1,20 @@
 package oop_00000069400_FarrelRamadaAlladines.week12
 
-// Custom exception dengan membawa data tambahan
 class InsufficientFundsException(
     val amount: Double,
     val balance: Double
 ): Exception("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
