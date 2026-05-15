5e0447105722604c02998d8e0af5349be81be26f
farre <farrelra202004@gmail.com>
2026-05-15 14:53:26 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
+++ src/oop_00000069400_FarrelRamadaAlladines/week12/SmartFeeder.kt
@@ -17,4 +17,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("Caught Hardware Error: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Caught Supply Error: ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: ${e.message}")
+    }
 }
