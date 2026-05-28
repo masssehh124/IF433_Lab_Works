e83dcfa9ec0f3e9ebc9b12e5f9542f7e7114da6f
farre <farrelra202004@gmail.com>
2026-05-28 22:34:45 +0700

    week13: create Student model

---
+++ src/oop_00000069400_FarrelRamadaAlladines/week13/StudentSerialization.kt
@@ -0,0 +1,7 @@
+package oop_00000069400_FarrelRamadaAlladines.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+// Definisikan kerangka data Student
+data class Student(val name: String, val age: Int, val gpa: Double)
